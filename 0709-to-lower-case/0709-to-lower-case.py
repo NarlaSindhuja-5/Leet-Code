b41c9f46ca7fa86acf81bd3f9970b7f4347dde00
NarlaSindhuja-5 <24eg107h02@anurag.edu.in>
2025-12-01 21:20:00 +0530

    Time: 0 ms (100%), Space: 17.6 MB (70.82%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        for i in s:
+            if(ord(i)>=65 and ord(i)<=90):
+                return s.lower()
+        return s
+        
+        
+        
