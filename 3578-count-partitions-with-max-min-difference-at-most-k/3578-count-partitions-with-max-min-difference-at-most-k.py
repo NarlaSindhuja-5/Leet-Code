e940dc3a34019bb29178cd4ca1d6c2a770e67901
NarlaSindhuja-5 <24eg107h02@anurag.edu.in>
2025-12-06 18:53:56 +0530

    Time: 454 ms (49.41%), Space: 26.8 MB (31.76%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.py
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.py
@@ -1,19 +1,35 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def countPartitions(self, nums: List[int], k: int) -> int:
-        mod=10**9+7
-        n=len(nums)
-        p=[0]*(n+1)
-        p[0]=1
-        for i in range(n):
-            current_min=float('inf')
-            current_max=float('-inf')
-            for j in range(i,-1,-1):
-                current_min = min(current_min, nums[j])
-                current_max = max(current_max, nums[j])
-                if current_max-current_min<=k:
-                    p[i+1]=(p[i+1]+p[j])%mod
-                else:
-                    break
-        return p[n]
-
-
+        MOD = 10**9 + 7
+        n = len(nums)
+        dp = [0] * (n + 1)
+        pref = [0] * (n + 1)
+        dp[0] = 1
+        pref[0] = 1
+        
+        minQ = deque()
+        maxQ = deque()
+        left = 0
+        
+        for right in range(n):
+            while minQ and nums[minQ[-1]] > nums[right]:
+                minQ.pop()
+            minQ.append(right)
+            
+            while maxQ and nums[maxQ[-1]] < nums[right]:
+                maxQ.pop()
+            maxQ.append(right)
+            
+            while nums[maxQ[0]] - nums[minQ[0]] > k:
+                left += 1
+                if minQ[0] < left:
+                    minQ.popleft()
+                if maxQ[0] < left:
+                    maxQ.popleft()
+            dp[right+1] = (pref[right] - (pref[left-1] if left > 0 else 0)) % MOD
+            pref[right+1] = (pref[right] + dp[right+1]) % MOD
+        
+        return dp[n]
